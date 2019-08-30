5b2021f707f530b9bd43b54e30fc6eb81bedbd8c
pchynoweth <pchynoweth@yahoo.com>
2019-08-30 14:42:47 +0100

    Added entrypoint.sh to ensure correct user is used.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM debian:buster
 
 RUN apt-get update && apt-get install -y osmium-tool
+
+COPY entrypoint.sh /root/entrypoint.sh
+
+ENTRYPOINT [ "/root/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+user=$(ls -dl . | awk '{ print $3; }')
+
+if [ "$user" == "root" ]; then
+    echo "Using root"
+    $@
+else
+    echo "Using non-privileged user"
+    addgroup --gid $(ls -dl . | awk '{ print $4; }') user
+    useradd --uid $user -g user user
+    runuser -u user $@
+fi
