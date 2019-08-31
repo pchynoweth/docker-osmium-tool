340be021092c32d71fb73d1af42fd680dcfb6315
pchynoweth <pchynoweth@yahoo.com>
2019-08-31 09:15:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ user=$(ls -dl . | awk '{ print $3; }')
 
 if [ "$user" == "root" ]; then
     echo "Using root"
-    $@
+    "$@"
 else
     echo "Using non-privileged user"
     addgroup --gid $(ls -dl . | awk '{ print $4; }') user
     useradd --uid $user -g user user
-    sudo -u user $@
+    sudo -u user "$@"
 fi
